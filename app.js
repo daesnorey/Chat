e5aad5ca5907098eafbffd0c693bb529c6a0ae16
Daniel Novoa <daniel.novoa@upsistemas.com>
2016-07-29 09:31:59 -0500

    [Edit] Modifico codigo, se mejoraron los eventos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
   "use strict";
 
+  const cryptico = require("cryptico");
   const app = require('express')(handler);
   const http = require('http').createServer(app);
   const io = require('socket.io')(http);
@@ -9,11 +10,14 @@
   const port = 8098;
 
   var users = [];
+  var messages = [];
   var user = { id: undefined, userName: undefined };
 
   http.listen( port );
 
   function handler (req, res) {
+    console.log( req );
+    console.log( res );
     //fs.readFile( __dirname + '/index.html', readFile );
   }
 
@@ -30,46 +34,65 @@
   app.get( '/', get );
 
   function get( req, res ){
-    res.sendFile( __dirname + '/index.html' );
+    //res.sendFile( __dirname + '/index.html' );
   };
 
   io.on( 'connection', userConnected );
 
   function userConnected( socket ){
+    //socket.conn.remoteAddress
     //console.log('a user connected', socket.id );
+    printMessages( socket.id );
+
     socket.on( 'disconnect', userDisconnect );
     socket.on( 'setdata', setData );
-    socket.on('chat message', function( msg ){
-      //console.log('message: ' + msg);
-      //console.log(socket.id);
-      let client = getClient( socket.id );
-      socket.broadcast.emit('chat message', { user: client.user, msg: msg } );
-    });
+    socket.on( 'chat message', chatMessage );
   };
+
+  function chatMessage( msg ){
+    let client = getClient( this.id );
+    this.broadcast.emit( 'chat message', { user: client.user, msg: msg } );
+    addMessage( { user: client.user, msg: msg } );
+  }
+
+  function addMessage( msg ){
+    messages.push( msg );
+  }
+
+  function printMessages( id ){
+    for( let i = 0; i < messages.length; i++ ){
+      let msg = messages[ i ];
+      console.log( msg );
+      io.to( id ).emit('chat message', msg );
+    }
+  }
 
   function getClient( socketId ){
     for( let i = 0; i < users.length; i++ ){
       var actual = users[ i ];
       if( actual.id == socketId ){
-        return { user: actual.userName, index: i };
+        actual.index = i;
+        return actual;
       }
     }
     return false;
   }
 
   function setData( data ){
-    let currentUser = getClient( this.id );
-    console.log( currentUser );
+    var currentUser = getClient( this.id );
     if( !currentUser ){
       let client = user.constructor();
       client.id = this.id;
-      client.userName = data.user;
-
+      client.user = data.user;
+      //client.key = keypair( { bits: "1024", e: "010001" } );
       users.push( client );
+      //io.emit('sData',
+          //{ prikey: client.key.private, pubkey: client.key.public } );
     }else{
-      users[ currentUser.index ].userName = data.user;
+      users[ currentUser.index ].user = data.user;
     }
-    console.log( users );
+    console.log( 'user connected', this.id );
+    //console.log( users );
   }
 
   function userDisconnect(){
@@ -80,9 +103,8 @@
         break;
       }
     }
-
     console.log( 'user disconnected', this.id );
-    console.log( users );
+    //console.log( users );
   }
 
 })();
